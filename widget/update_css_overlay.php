44141af6f078e125bb443554c1d7d817b5e8461a
moisesroihero <moises.dourado@roihero.com.br>
2018-11-30 16:07:58 -0200

    ajsute caminho dos arquivos

--- widget/update_css_overlay.php
+++ widget/update_css_overlay.php
@@ -17,7 +17,7 @@
         // Pega o arquivo css e substitui as cores
         echo $css = @file_get_contents('templates/overlay/kit_'.$template.'/style_to_replace.css');
 
-        if(empty($css))
+        if(!empty($css))
         {
             echo "\n^-------------------------ATUALIZOU";
 
@@ -26,7 +26,7 @@
             $css = str_replace( '{PRIMARY_COLOR}' , $cor['primary'] , $css );
             $css = str_replace( '{SECONDARY_COLOR}' , $cor['secondary'] , $css );
 
-            //file_put_contents('css/overlay/rh_overlay_'.sha1($idCli).'.css',$css );
+            file_put_contents('css/overlay/rh_overlay_'.sha1($idCli).'.css',$css );
 
             //da purge no cache com a cloudflare
             $api = new cloudflare_api('moises.dourado@roihero.com.br','1404cc5e783d0287897bfb2ebf7faa9e87eb5');
