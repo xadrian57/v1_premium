27d9d8ee8e11e5482164751fc1efc94fe737e932
Eliabe França <eliabe.fc@gmail.com>
2019-02-23 18:09:11 -0300

    ajuste paulo email

--- php/contato.php
+++ php/contato.php
@@ -6,16 +6,15 @@ $email = $_POST['email'];
 $site = $_POST['site'];
 $mensagem = $_POST['mensagem'];
 
-$headers .= "Reply-To: ROI HERO <atendimento@roihero.com.br>\r\n";
-$headers .= "Return-Path: Roi Hero <atedimento@roihero.com.br>\r\n";
-$headers .= "From: Roi Hero <atendimento@roihero.com.br>\r\n";
+$headers .= "Reply-To: ROI HERO <acesso@roihero.com.br>\r\n";
+$headers .= "Return-Path: Roi Hero <acesso@roihero.com.br>\r\n";
+$headers .= "From: Roi Hero <acesso@roihero.com.br>\r\n";
 $headers .= "Organization: Roi Hero - Recomendações Inteligentes\r\n";
 $headers .= "MIME-Version: 1.0\r\n";
 $headers .= "X-Priority: 3\r\n";
 $headers .= "X-Mailer: PHP". phpversion() ."\r\n";
 
 $message = "Houve uma nova tentativa de contato através do site, os dados do lead são os seguintes:\n\nNome: ".$nome."\nE-mail: ".$email."\nTelefone: ".$telefone."\nSite: ".$site."\nPlataforma: ".$plataforma."\n\nE deixou a mensagem: ".$mensagem.".";
-mail('paulo.castello.branco@roihero.com.br' , "Novo Contato pelo Site",$message,$headers);
+mail('test-a3ahx@mail-tester.com' , "Novo Contato pelo Site",$message,$headers);
 
 ?>
-           
