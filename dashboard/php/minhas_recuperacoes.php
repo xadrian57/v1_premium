e5a59e96178f3da4bc40fd956689b34c0929bdbb
Eliabe <eliabe.fc@gmail.com>
2019-04-15 18:09:14 -0300

    pagina minhas recuperacoes sendo adicionada

---
+++ dashboard/php/minhas_recuperacoes.php
@@ -0,0 +1,10 @@
+<?php
+	// DADOS DA PÁGINA 
+	$pagina_nome = 'minha_busca';
+	$pagina_titulo = 'Minhas Buscas';
+
+    require_once('../resource/resource_verifica_sessao.php');    
+	require_once('../content/content_header.html');
+	require_once('../content/content_minhas_recuperacoes.html');
+	require_once('../content/content_footer.html');
+?>
