f4bec6dc4c1224ec1967d13a2484e64d6ad0a79f
Eliabe <eliabe.fc@gmail.com>
2019-04-16 16:56:42 -0300

    pagina smart recovery

--- dashboard/assets/js/pages/minhas_recuperacoes/minhas_recuperacoes.js
+++ dashboard/assets/js/pages/minhas_recuperacoes/minhas_recuperacoes.js
@@ -11,19 +11,39 @@ $(document).ready(function() {
 				success: function ( result )
 				{
 					var data = JSON.parse(result)
-					console.log(data);					
+					console.log(data);
+
+					this.loadRecBoleto(data.boleto)
+					this.loadRecCarrinho(data.carrinho)
 				}
 			})
 		
 		},
 
-		loadRecBoleto: function() {
-			var widgetsBoleto = document.getElementById('widgetsBoleto');
-			widgetsProduto.innerHTML = "";
-			produto.forEach(function (wid) {
+		loadRecBoleto: function(wids) {
+			var $list = document.getElementById('widgetsBoleto');
+			wids.innerHTML = "";
+			wids.forEach(function (wid) {
 				var ativo = (wid.ativo === '1') ? 'checked' : '';
 
-				widgetsProduto.innerHTML = widgetsProduto.innerHTML +
+				$list.innerHTML = $list.innerHTML +
+					'<li class="list-group-item" wid-id="' + wid.id + '"><span>' + wid.nome + '</span>' +
+					'<div style="width: auto;display: inline-block;position:relative;bottom: 7px;float:right;">' +
+					'<!-- <button class="btn btn-danger pull-right" data-delete-wid=' + wid.id + '><i class="ft-x"></i> Deletar</button> -->' +
+					'<button class="btn btn-info pull-right mr-1 ml-1 btn-edita-wid"><i class="icon-pencil"></i> Editar</button>' +
+					'<input type="checkbox" class="switch pull-right" data-off-label="desativar" data-on-label="ativar" data-switch-always ' + ativo + '/>' +
+					'</div>' +
+					'</li>';
+			});
+		},
+
+		loadRecCarrinho: function(wids) {
+			var $list = document.getElementById('widgetsCarrinho');
+			wids.innerHTML = "";
+			wids.forEach(function (wid) {
+				var ativo = (wid.ativo === '1') ? 'checked' : '';
+
+				$list.innerHTML = $list.innerHTML +
 					'<li class="list-group-item" wid-id="' + wid.id + '"><span>' + wid.nome + '</span>' +
 					'<div style="width: auto;display: inline-block;position:relative;bottom: 7px;float:right;">' +
 					'<!-- <button class="btn btn-danger pull-right" data-delete-wid=' + wid.id + '><i class="ft-x"></i> Deletar</button> -->' +
