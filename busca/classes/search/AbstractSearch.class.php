387c6bb07e5c1a8a41555355a4a203c75e027e70
moisesroihero <moises.dourado@roihero.com.br>
2019-05-09 13:45:59 -0300

    Correção ordenação estoque

--- busca/classes/search/AbstractSearch.class.php
+++ busca/classes/search/AbstractSearch.class.php
@@ -198,7 +198,8 @@ abstract class AbstractSearch
 		$select = "SELECT ". $this->campos() ."
            FROM XML_".$this->idcli."
            WHERE (XML_availability = 1 OR XML_availability = 0)
-           AND XML_id IN ('". $ids ."')";	
+           AND XML_id IN ('". $ids ."')
+           ORDER BY XML_availability DESC";	
     
     	$result = mysqli_query($this->conDados, $select);
 
